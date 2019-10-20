c02842b61e1f5c9cb74ca4338010b4d92fd1e9fa
Foks991 <33574193+Foks991@users.noreply.github.com>
2019-10-20 23:55:56 +0300

    Js Calc + C# Calc

---
+++ OOP Calc Js/js/logic.js
@@ -0,0 +1,23 @@
+function summ(a, b) {
+	a = ((a * 1000000000) + (b * 1000000000))/1000000000;
+	return a;
+}
+function minus(a, b) {
+	a = ((a * 1000000000) - (b * 1000000000))/1000000000;
+	return a;
+}	
+function divide(a, b) {
+	a = ((a * 1000000000) / (b * 1000000000));
+	return a; 
+}
+function multiply(a, b) {
+	a = ((a * 1000000) * (b * 1000))/1000000000;
+	return a;
+}
+
+let logic = {
+	summ,
+	minus,
+	divide,
+	multiply,
+};
